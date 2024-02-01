e84523372e305933cf6f076a97eda13aa6fd6faf
Abd Ur Rehman <germany61907@gmail.com>
2024-02-01 20:27:50 +0500

    header and footer

--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -1,20 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <>
-      <header className="p-4 text-black border-b">
+      <header className="p-4 text-black border-b flex justify-center">
         <div className="container mx-auto flex justify-between items-center">
           <div className="text-2xl font-bold">Your Logo</div>
 
           <nav className="hidden md:flex space-x-4">
-            <a href="#" className="hover:text-gray-300">
+            <Link to="/" className="hover:text-gray-300">
               Home
-            </a>
-            <a href="#" className="hover:text-gray-300">
+            </Link>
+            <Link to="/contact" className="hover:text-gray-300">
               Contact
-            </a>
-            <a href="#" className="hover:text-gray-300">
+            </Link>
+            <Link to="/about" className="hover:text-gray-300">
               About
-            </a>
+            </Link>
           </nav>
 
           <div className="md:flex items-center">
@@ -24,12 +26,12 @@ export default function Header() {
               className="bg-[#F5F5F5] text-white p-2 rounded-md focus:outline-none"
             />
 
-            <a href="#" className="ml-4 hover:text-gray-300">
+            <Link to="/signup" className="ml-4 hover:text-gray-300">
               SignUp
-            </a>
-            <a href="#" className="ml-2 hover:text-gray-300">
+            </Link>
+            <Link to="/login" className="ml-2 hover:text-gray-300">
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </header>
