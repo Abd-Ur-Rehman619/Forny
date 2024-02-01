12075bf6013a3d7ddf3aca1a1e52d21c5e2fac01
Tahir Mehmood <tahir.meh012@gmail.com>
2024-02-01 22:36:51 +0500

    build a login page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,12 @@
-import Signup from "./signup/Signup";
+// import Signup from "./signup/Signup";
+
+import Login from "./login/Login";
 
 export default function Home() {
   return (
     <>
-      <Signup />
+      {/* <Signup /> */}
+      <Login />
     </>
   );
 }
