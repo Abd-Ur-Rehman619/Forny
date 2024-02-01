19ca837e9e311fa625a133c4fcfcecafcd2fe66e
Abd Ur Rehman <germany61907@gmail.com>
2024-02-01 22:48:30 +0500

    Home page continue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,7 @@
 export default function Home() {
   return (
     <>
-      <section className="p-4 text-black border-b flex justify-center">
-        <div className="container mx-auto flex justify-between items-center">
-          halo
-        </div>
-      </section>
+      <div>home</div>
     </>
   );
 }
